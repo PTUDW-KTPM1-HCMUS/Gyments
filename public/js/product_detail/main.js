031ad245cb26bab98713e493212b736a428c1a1c
CYPHER-69\ADMIN <hoanghuugiap241.github.io>
2021-11-20 18:10:04 +0700

    update image slider in product detail

--- public/js/product_detail/main.js
+++ public/js/product_detail/main.js
@@ -5,11 +5,9 @@
     -----------------------------------------------------*/
     $('.hot-deal-active').owlCarousel({
         loop: false,
-        nav: true,
         dots: false,
         smartSpeed: 1500,
-        navText: ["<i class=\"fa fa-arrow-left\" aria-hidden=\"true\"></i>", "<i class=\"fa fa-arrow-right\" aria-hidden=\"true\"></i>"],
-        margin: 10,
+        margin: 12,
         responsive: {
             0: {
                 items: 1,
@@ -29,64 +27,8 @@
                 items: 5
             }
         }
-    })
-    // $('.hot-deal-active3').owlCarousel({
-    //     loop: false,
-    //     nav: true,
-    //     dots: false,
-    //     smartSpeed: 1500,
-    //     navText: ["<i class=\"fa fa-arrow-left\" aria-hidden=\"true\"></i>", "<i class=\"fa fa-arrow-right\" aria-hidden=\"true\"></i>"],
-    //     margin: 10,
-    //     responsive: {
-    //         0: {
-    //             items: 1,
-    //             autoplay: true,
-    //             smartSpeed: 500
-    //         },
-    //         480: {
-    //             items: 1
-    //         },
-    //         768: {
-    //             items: 1
-    //         },
-    //         992: {
-    //             items: 1
-    //         },
-    //         1200: {
-    //             items: 1
-    //         }
-    //     }
-    // })
 
-    /*-------------------------------------
-    Thumbnail Product activation
-    --------------------------------------*/
-    $('.thumb-menu').owlCarousel({
-        loop: false,
-        // navText: ["<i class='lnr lnr-arrow-left'></i>", "<i class='lnr lnr-arrow-right'></i>"],
-        navText: ["<i class=\"arrow_slider fa fa-arrow-left\" aria-hidden=\"true\"></i>", "<i class=\"arrow_slider fa fa-arrow-right\" aria-hidden=\"true\"></i>"],
-        margin: 15,
-        smartSpeed: 1000,
-        nav: true,
-        dots: false,
-        responsive: {
-            0: {
-                items: 3,
-                autoplay: true,
-                smartSpeed: 500
-            },
-            768: {
-                items: 3
-            },
-            1000: {
-                items: 3
-            }
-        }
     })
-    $('.thumb-menu a').on('click', function () {
-        $('.thumb-menu a').removeClass('active');
-    })
-
 })(jQuery);
 
 
