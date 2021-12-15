ae633ace7712f7918048b9b9a2b5d34df1e443ab
Viet.68 <tanviet060801@gmail.com>
2021-12-15 19:08:32 +0700

    Update layout, add search products by category

--- components/products/ProductController.js
+++ components/products/ProductController.js
@@ -1,5 +1,5 @@
 const service = require('./ProductService');
-
+const categoryService = require("../categories/categoryService");
 class ProductController{
     async getAllProduct(req, res){
         try{
@@ -7,7 +7,9 @@ class ProductController{
             const [products, pages] = await service.getAllProduct(currentPage);
             let previous = Math.ceil(parseInt(currentPage)-1)<1? 1:Math.ceil(parseInt(currentPage)-1);
             let next = Math.ceil(parseInt(currentPage)+1) > pages.length?pages.length: Math.ceil(parseInt(currentPage)+1);
-            res.render('products/views/product',{products, pages, currentPage,previous,next});
+            let categories = await categoryService.getAllCategories();
+            res.render('products/views/product',{products, pages, currentPage,previous,next, categories});
+
         }catch(err){
             console.log({message: err});
         }
@@ -18,6 +20,24 @@ class ProductController{
             let newPrice = productDetails.price - productDetails.price * productDetails.sale / 100;
             res.render('products/views/productDetail', {productDetails, relatedProducts, newPrice});
         }catch (err) {
+            console.log({message: err});
+        }
+    }
+    async getAllByCategory(req,res){
+        try{
+            const categoryID = await categoryService.getCategoryIDByID(req.params.ID);
+            if (!categoryID){
+                let categories = await categoryService.getAllCategories();
+                res.render('products/views/product',{categories});
+            }
+            let currentPage = req.query.page || 1;
+            const [products, pages] = await service.getByCategoryID(categoryID, currentPage);
+            let previous = Math.ceil(parseInt(currentPage)-1)<1? 1:Math.ceil(parseInt(currentPage)-1);
+            let next = Math.ceil(parseInt(currentPage)+1) > pages.length?pages.length: Math.ceil(parseInt(currentPage)+1);
+            let categories = await categoryService.getAllCategories();
+            res.render('products/views/product',{products, pages, currentPage,previous,next, categories});
+
+        }catch(err){
             console.log({message: err});
         }
     }
--- components/products/ProductService.js
+++ components/products/ProductService.js
@@ -63,4 +63,38 @@ const getProduct = async (productID) => {
     }
     return [productDetails, relatedProducts];
 }
-module.exports = {getAllProduct , getProduct};
+const getByCategoryID = async (categoryID, reqPage)=>{
+    let products = [];
+    let pages = [];
+    try{
+        products = await Product.find({'categoryID':categoryID}).lean();
+        const perPage = 6;
+        const page = parseInt(reqPage);
+
+        const pro_start = (page - 1) * perPage;
+        const pro_end = page * perPage;
+
+        for (let i = 0; i < products.length / perPage; i++){
+            let tmp = {};
+            tmp.currentPage = i + 1;
+            tmp.pageLink = `?page=${i+1}`;
+            pages.push(tmp);
+        }
+        products = products.slice(pro_start, pro_end);
+
+        products = products.map(item => {
+            let name = item.name;
+            let rate = new Array(item.rate).fill(0);
+            if (item.name.length >= 30)
+                name = item.name.slice(0, 28) + "...";
+            let productID = "/product/" + item._id;
+            return { ...item, name: name, productID: productID, rate: rate }
+        });
+
+        return [products, pages];
+    }catch(err){
+        console.log({message: err});
+    }
+    return [products, pages]
+}
+module.exports = {getAllProduct , getProduct, getByCategoryID};
